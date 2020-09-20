5fc5ee1bfb492514e6291c2b653913f04e114717
iBessonob <bessonov3145@gmail.com>
2020-09-20 01:50:16 -0700

    Webapp can fetch data from firebase

--- website/src/components/Services.vue
+++ website/src/components/Services.vue
@@ -5,6 +5,26 @@
 </template>
 
 <script>
+
+//firebase setup
+import * as firebase from 'firebase';
+
+console.log("BLAHALHASLHLHLAHSDLFHASDF");
+let app = firebase.initializeApp({
+  databaseURL: 'https://hackmit-df9ea.firebaseio.com/'
+});
+let dbRef = app.database().ref('SensorData');
+
+dbRef.once("value").then(function(snapshot){
+  const datab = snapshot.val();
+  console.log("Fire: ", datab.FIRE);
+  console.log("LPG: ", datab.LPG);
+  console.log("Monoxide: ", datab.Monoxide);
+  console.log("Temp: ", datab.Temperature);
+  console.log("timehour: ", datab.timehour);
+});
+console.log("Successful");
+
 export default {
   name: 'Services',
   props: {
@@ -23,7 +43,4 @@ export default {
     color: #bb86fc;
     font-family: kanit;
   }
-
-  
-
 </style>
