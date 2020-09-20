ffd7c00d9772f6285bf5743c65c8a6524cf56741
iBessonob <bessonov3145@gmail.com>
2020-09-20 00:21:03 -0700

    Backedup website. Title padding glitchy

---
+++ website/src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <div id="logo">
+      <img id="fire-logo" alt="Fire logo" src="./assets/fire.png">
+    </div>
+    <div id="title">
+      <h1>Vacuate</h1>
+    </div>
+    <Services msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import Services from './components/Services.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Services
+  }
+}
+</script>
+
+<style>
+
+/*Main fire logo styling*/
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 1%;
+  display: flex;
+}
+
+/* Fire Logo size */
+#fire-logo{
+  width: 16%;
+  height: 90%;
+}
+
+#logo{
+  margin-top: 3%;
+  margin-right: 0;
+  margin-left: 20%;
+  padding: 0;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #bb86fc;
+}
+
+#title{
+  margin-top: 3%;
+  margin-left: 0;
+  font-family:  Arial, Helvetica, sans-serif;
+  color: #bb86fc;
+}
+
+/* Set overall bg to shade of grey*/
+body{
+  background: #262a2e;
+}
+</style>
---
+++ website/src/components/Services.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="intro">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Services',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  body{
+    background: #262a2e;
+  }
+
+  .intro{
+    color: #bb86fc;
+    font-family: kanit;
+  }
+
+  
+
+</style>
